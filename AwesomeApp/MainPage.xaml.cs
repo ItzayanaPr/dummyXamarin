6e9aa77c368c022f8922d725149493a040a6a392
ItzayanaPr <itzayana.prado@undostres.com.mx>
2022-02-23 13:08:40 -0600

    First Commit - Card and btns without action

---
+++ AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using AwesomeApp.ViewModels;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new VMMainPage();
+            NavigationPage.SetHasNavigationBar(this, true);
+            this.Title = "Xamarin buttons";
+        }
+        
+    }
+}
